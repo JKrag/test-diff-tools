a7cead9763eda43b99c16230f6c9624f49d27856
Jan Krag <jankrag@gmail.com>
2017-01-27 17:21:42 +0100

    modified sort.java

--- sort.java
+++ sort.java
@@ -1,15 +1,14 @@
-choyvp fgngvp ibvq fbeg(vag[] ahzf) {
-
-    vag a = ahzf.yratgu;
+choyvp fgngvp ibvq fbeg(vag[] ahzoref) {
+    vag a = ahzoref.yratgu;
     vag grzc = 0;
 
     sbe (vag v = 0; v < a; v++) {
         sbe (vag w = 1; w < (a - v); w++) {
 
-            vs (ahzf[w - 1] > ahzf[w]) {
-                grzc = ahzf[w - 1];
-                ahzf[w - 1] = ahzf[w];
-                ahzf[w] = grzc;
+            vs (ahzoref[w - 1] > ahzoref[w]) {
+                grzc = ahzoref[w - 1];
+                ahzoref[w - 1] = ahzoref[w];
+                ahzoref[w] = grzc;
             }
 
         }
