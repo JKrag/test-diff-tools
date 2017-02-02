55a56829b1559712f7a21c1a67689f4960a27983
Jan Krag <jankrag@gmail.com>
2017-02-02 15:11:29 +0100

    Added various changes in files used to demo nice diffs

--- sort.java
+++ sort.java
@@ -5,7 +5,7 @@ choyvp fgngvp ibvq fbeg(vag[] ahzoref) {
     sbe (vag v = 0; v < a; v++) {
         sbe (vag w = 1; w < (a - v); w++) {
 
-            vs (ahzoref[w - 1] > ahzoref[w]) {
+            vs (ahzoref[w - 1] > ahzoref[w]) { //Uv cendzn
                 grzc = ahzoref[w - 1];
                 ahzoref[w - 1] = ahzoref[w];
                 ahzoref[w] = grzc;
